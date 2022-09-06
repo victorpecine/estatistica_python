0812bd0c4a0f7e06c03f42dae2f0473df83efa75
Victor Pecine <73555379+victorpecine@users.noreply.github.com>
2022-09-06 11:41:35 -0300

    cruzamento de tabelas

--- dados_estatisticos.py
+++ dados_estatisticos.py
@@ -43,6 +43,48 @@ dist_freq_sexo = pd.DataFrame({'frequencia_sexo': freq_sexo, 'porcentual_sexo':p
 dist_freq_sexo.rename_axis('sexo', axis='columns', inplace=True)
 dist_freq_sexo.rename(index={0: 'masculino', 1: 'feminino'}, inplace=True)
 
-print(dist_freq_sexo)
 
-dist_freq_sexo.to_csv('dados/df_sexo.csv')
+# Cruzamento de tabelas
+sexo = {0: 'masculino', 1: 'feminino'}
+
+cor = {0:'indigena',
+       2: 'branca',
+       4: 'preta',
+       6: 'amarela',
+       8: 'parda',
+       9: 'sem declaracao'}
+
+freq_sexo_cor = pd.crosstab(df_ibge['sexo'],
+                            df_ibge['cor'])
+
+freq_sexo_cor.rename(index=sexo, columns=cor, inplace=True)
+# cor        indigena  branca  preta  amarela  parda
+# sexo
+# masculino       256   22194   5502      235  25063
+# feminino        101    9621   2889      117  10862
+
+
+porcent_sexo_cor = pd.crosstab(df_ibge['sexo'],
+                               df_ibge['cor'],
+                               normalize=True) * 100
+
+porcent_sexo_cor.rename(index=sexo, columns=cor, inplace=True)
+# cor        indigena     branca     preta   amarela      parda
+# sexo
+# masculino  0.333160  28.883394  7.160333  0.305830  32.617126
+# feminino   0.131442  12.520822  3.759761  0.152264  14.135867
+
+
+renda_media_sexo_cor =  pd.crosstab(df_ibge['sexo'],
+                                    df_ibge['cor'],
+                                    aggfunc='mean',
+                                    values=df_ibge['renda'])
+
+renda_media_sexo_cor.rename(index=sexo, columns=cor, inplace=True)
+# cor           indigena       branca        preta      amarela        parda
+# sexo
+# masculino  1081.710938  2925.744435  1603.861687  4758.251064  1659.577425
+# feminino   2464.386139  2109.866750  1134.596400  3027.341880  1176.758516
+
+renda_media_sexo_cor = renda_media_sexo_cor.round(2)
+print(renda_media_sexo_cor)
