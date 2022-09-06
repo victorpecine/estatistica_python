33a64e22b4bd30993d6a018be61478e63c1a2615
Victor Pecine <73555379+victorpecine@users.noreply.github.com>
2022-09-06 11:19:00 -0300

    analises qualitativas e criacao dataframe sexo

--- dados_estatisticos.py
+++ dados_estatisticos.py
@@ -1,3 +1,4 @@
+from dis import dis
 import pandas as pd
 
 
@@ -13,9 +14,35 @@ df_ibge.isnull().sum()
 # Renda             0
 # Altura            0
 
-# Ajuste de nomes das colunas
+# Ajuste dos nomes das colunas
 df_ibge.columns = df_ibge.columns.str.lower()
 
 df_ibge.rename(columns={'anos de estudo': 'anos_estudo'}, inplace=True)
 
-print(df_ibge)
+
+# Análises iniciais
+idade_min = df_ibge['idade'].min() # 13
+
+idade_max = df_ibge['idade'].max() # 99
+
+altura_min = df_ibge['altura'].min() # 1.339244614 metros
+
+altura_max = df_ibge['altura'].max() # 2.028496765 metros
+
+freq_sexo = df_ibge['sexo'].value_counts()
+# 0    53250 - homens
+# 1    23590 - mulheres
+
+porcent_sexo = df_ibge['sexo'].value_counts(normalize=True) * 100
+# 0    69.299844%
+# 1    30.700156%
+
+# Distribuição de frequência
+dist_freq_sexo = pd.DataFrame({'frequencia_sexo': freq_sexo, 'porcentual_sexo':porcent_sexo})
+
+dist_freq_sexo.rename_axis('sexo', axis='columns', inplace=True)
+dist_freq_sexo.rename(index={0: 'masculino', 1: 'feminino'}, inplace=True)
+
+print(dist_freq_sexo)
+
+dist_freq_sexo.to_csv('dados/df_sexo.csv')
