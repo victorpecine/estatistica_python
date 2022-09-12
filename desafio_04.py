d5798c86c71605e6c3f4b6492cd68f36e14fbfbd
Victor Pecine <73555379+victorpecine@users.noreply.github.com>
2022-09-12 16:33:47 -0300

    desafio_4_c

--- desafio_04.py
+++ desafio_04.py
@@ -16,28 +16,26 @@ media = 1.7
 
 desv_padrao = 0.1
 
-a_superior = 1.8
+a = 1.9
 
-z_superior = (a_superior - media) / desv_padrao
+z = (a - media) / desv_padrao
 # 1.0000000000000009
 
-probabilidade_superior = norm.cdf(z_superior)
+probabilidade = norm.sf(z)
 # 0.8413447460685431
 # A. probabilidade de uma pessoa, selecionada ao acaso, ter menos de 1,80 metros é de 84,13%
 
-
-a_inferior = 1.6
-
-z_inferior = (a_inferior - media) / desv_padrao
-print(z_inferior)
-# -0.9999999999999987
-
-probabilidade_inferior = norm.cdf(z_inferior)
-print(probabilidade_inferior)
-# 0.1586552539314574
-
-probabilidade = norm.cdf(z_superior) - norm.cdf(z_inferior)
-
+print(z)
 print(probabilidade)
-# 0.6826894921370857
-# B. probabilidade de uma pessoa, selecionada ao acaso, ter entre 1,60 metros e 1,80 metros é de 68,26%
+
+# a_inferior = 1.6
+
+# z_inferior = (a_inferior - media) / desv_padrao
+# # -0.9999999999999987
+
+# probabilidade_inferior = norm.cdf(z_inferior)
+# # 0.1586552539314574
+
+# probabilidade = norm.cdf(z_superior) - norm.cdf(z_inferior)
+# # 0.6826894921370857
+# # B. probabilidade de uma pessoa, selecionada ao acaso, ter entre 1,60 metros e 1,80 metros é de 68,26%
