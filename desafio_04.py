5fd97c1d656e70f73d00f3d6e318d0b6e3b1eb3b
Victor Pecine <73555379+victorpecine@users.noreply.github.com>
2022-09-12 16:22:16 -0300

    desafio_4_b

--- desafio_04.py
+++ desafio_04.py
@@ -11,32 +11,33 @@ import pandas as pd
 import numpy as np
 from scipy.stats import norm
 
-# Tabela padronizada
-tabela_normal_padronizada = pd.DataFrame(
-    [], 
-    index=["{0:0.2f}".format(i / 100) for i in range(0, 400, 10)],
-    columns = ["{0:0.2f}".format(i / 100) for i in range(0, 10)])
-
-for index in tabela_normal_padronizada.index:
-    for column in tabela_normal_padronizada.columns:
-        Z = np.round(float(index) + float(column), 2)
-        tabela_normal_padronizada.loc[index, column] = "{0:0.4f}".format(norm.cdf(Z))
-
-tabela_normal_padronizada.rename_axis('Z', axis = 'columns', inplace = True)
-
-
 # Variável padronizada Z
 media = 1.7
 
 desv_padrao = 0.1
 
-a = 1.8
+a_superior = 1.8
 
-z = (a - media) / desv_padrao
+z_superior = (a_superior - media) / desv_padrao
 # 1.0000000000000009
 
-probabilidade = norm.cdf(z)
+probabilidade_superior = norm.cdf(z_superior)
 # 0.8413447460685431
 # A. probabilidade de uma pessoa, selecionada ao acaso, ter menos de 1,80 metros é de 84,13%
 
+
+a_inferior = 1.6
+
+z_inferior = (a_inferior - media) / desv_padrao
+print(z_inferior)
+# -0.9999999999999987
+
+probabilidade_inferior = norm.cdf(z_inferior)
+print(probabilidade_inferior)
+# 0.1586552539314574
+
+probabilidade = norm.cdf(z_superior) - norm.cdf(z_inferior)
+
 print(probabilidade)
+# 0.6826894921370857
+# B. probabilidade de uma pessoa, selecionada ao acaso, ter entre 1,60 metros e 1,80 metros é de 68,26%
