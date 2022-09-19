77935c5ae65eac4468eba45a48ffc9ef7c358734
Victor Pecine <73555379+victorpecine@users.noreply.github.com>
2022-09-19 15:55:23 -0300

    exemplo hipotese nula H0

--- testes_hipoteses.py
+++ testes_hipoteses.py
@@ -12,3 +12,15 @@ df_ibge.isnull().sum()
 df_ibge.columns = df_ibge.columns.str.lower()
 
 df_ibge.rename(columns={'anos de estudo': 'anos_estudo'}, inplace=True)
+
+
+significancia = 0.05
+
+
+# Teste da hipótese nula H0 para renda
+stat_test, p_valor = normaltest(df_ibge['altura'])
+
+if p_valor <= significancia:
+    print('A amostra não é proveniente de uma distribuição normal\np_valor = {}\nDesconsiderar a hipótese nula H0'.format(p_valor))
+else:
+    print('A amostra é proveniente de uma distribuição normal e seu p_valor é {}'.format(p_valor))
