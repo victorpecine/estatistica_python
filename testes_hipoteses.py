1593a669585f32fb7d4d1e49acfcd5d29fa4bc44
Victor Pecine <73555379+victorpecine@users.noreply.github.com>
2022-09-19 14:40:02 -0300

    criacao do dataframe

---
+++ testes_hipoteses.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import normaltest
+
+
+df_ibge = pd.read_csv('dados/dados.csv') # 76840 linhas, 7 colunas
+
+# Contagem de dados nulos do dataframe
+df_ibge.isnull().sum()
+
+# Ajuste dos nomes das colunas
+df_ibge.columns = df_ibge.columns.str.lower()
+
+df_ibge.rename(columns={'anos de estudo': 'anos_estudo'}, inplace=True)
