d2848cb57a1f6eae0b7c055cfdf1c370e18a6659
Victor Pecine <73555379+victorpecine@users.noreply.github.com>
2022-09-22 14:33:57 -0300

    teste de mann-whitney_unicaudal inferior_valor critico t

--- testes_nao_parametricos.py
+++ testes_nao_parametricos.py
@@ -1,110 +1,143 @@
-# Um novo tratamento para acabar com o hábito de fumar está sendo empregado em um grupo de 35 pacientes voluntários. De cada paciente testado foram obtidas as informações de quantidades de cigarros consumidos por dia antes e depois do término do tratamento. Assumindo um nível de confiança de 95% é possível concluir que, depois da aplicação do novo tratamento, houve uma mudança no hábito de fumar do grupo de pacientes testado?
+# Em nosso dataset temos os rendimento dos chefes de domicílio obtidos da Pesquisa Nacional por Amostra de Domicílios - PNAD no ano de 2015. Um problema bastante conhecido em nosso país diz respeito a desigualdade de renda, principalmente entre homens e mulheres.
+
+# Duas amostras aleatórias, uma de 6 homens e outra com 8 mulheres, foram selecionadas em nosso dataset. Com o objetivo de comprovar tal desigualdade teste a igualdade das médias entra estas duas amostras com um nível de significância de 5%.
 
 
 import pandas as pd
-from scipy.stats import norm, wilcoxon
+from scipy.stats import t as t_student
 import numpy as np
 
 
-fumo = {
-    'antes': [39, 25, 24, 50, 13, 52, 21, 29, 10, 22, 50, 15, 36, 39, 52, 48, 24, 15, 40, 41, 17, 12, 21, 49, 14, 55, 46, 22, 28, 23, 37, 17, 31, 49, 49],
-    'depois': [16, 8, 12, 0, 14, 16, 13, 12, 19, 17, 17, 2, 15, 10, 20, 13, 0, 4, 16, 18, 16, 16, 9, 9, 18, 4, 17, 0, 11, 14, 0, 19, 2, 9, 6]
-}
+df_ibge = pd.read_csv('dados/dados.csv') # 76840 linhas, 7 colunas
+
+# Contagem de dados nulos do dataframe
+df_ibge.isnull().sum()
+
+# Ajuste dos nomes das colunas
+df_ibge.columns = df_ibge.columns.str.lower()
+
+df_ibge.rename(columns={'anos de estudo': 'anos_estudo'}, inplace=True)
+
+
+mulheres = df_ibge.query('sexo == 1 and renda > 0').sample(n=8, random_state=101).renda
+
+homens = df_ibge.query('sexo == 0 and renda > 0').sample(n=6, random_state=101).renda
+
+
+media_amostra_M = mulheres.mean()
+# 1090.75
+
+media_amostra_H = homens.mean()
+# 1341.6666666666667
+
 
 significancia = 0.05
 
 confianca = 1 - significancia
 
-n = 35
+n_1 = len(homens) # Número de elementos do menor grupo
 
-df_fumo = pd.DataFrame(fumo)
-
-media_antes = df_fumo['antes'].mean()
-# 31.857142857142858
-
-media_depois = df_fumo['depois'].mean()
-# 11.2
+n_2 = len(mulheres) # Número de elementos do maior grupo
 
 
-# Formulação das hipóteses
-# H0 -> media_antes = media_depois
-# H1 -> media_antes > media_depois
+# Hipóteses
+# H0 -> media_amostra_M = media_amostra_H
+# H1 -> media_amostra_M < media_amostra_H
+
+# Deve-se optar pela distribuição t de Student, já que nada é mencionado sobre a distribuição da população, o desvio padrão populacional é desconhecido e o número de elementos investigados é menor que 30.
 
 
-probabilidade = (0.5 + (confianca / 2))
-# 0.975
-
-z_alpha_2 = norm.ppf(probabilidade)
-# 1.959963984540054
+graus_de_liberdade = n_1 + n_2 - 2
 
 
-df_fumo['diferenca'] = df_fumo['depois'] - df_fumo['antes']
+tabela_t_student = pd.DataFrame(
+    [], 
+    index=[i for i in range(1, 31)],
+    columns = [i / 100 for i in range(10, 0, -1)]
+)
 
-df_fumo['|diferenca|'] = df_fumo['diferenca'].abs()
+for index in tabela_t_student.index:
+    for column in tabela_t_student.columns:
+        tabela_t_student.loc[index, column] = t_student.ppf(1 - float(column) / 2, index)
 
-df_fumo.sort_values(by = '|diferenca|', inplace = True)
+index=[('Graus de Liberdade (n - 1)', i) for i in range(1, 31)]
+tabela_t_student.index = pd.MultiIndex.from_tuples(index)
 
-df_fumo['#'] = range(1, len(df_fumo) + 1)
+columns = [("{0:0.3f}".format(i / 100), "{0:0.3f}".format((i / 100) / 2)) for i in range(10, 0, -1)]
+tabela_t_student.columns = pd.MultiIndex.from_tuples(columns)
 
+tabela_t_student.rename_axis(['Bicaudal', 'Unicaudal'], axis=1, inplace = True)
 
-df_media_grupos = df_fumo[['|diferenca|', '#']].groupby(['|diferenca|']).mean()
+# print(tabela_t_student[10:13])
 
-df_media_grupos.rename(columns={'#': 'media_'}, inplace=True)
-
-df_media_grupos.reset_index(inplace=True) # Cria index
-
-
-df_fumo.drop(['#'], axis = 1, inplace = True) # Apaga a coluna #
-
-df_fumo = df_fumo.merge(df_media_grupos, left_on='|diferenca|', right_on='|diferenca|', how = 'left') # Left join utilizando |diferenca|
-
-
-df_fumo['media_ (+)'] = df_fumo.apply(lambda x: x.media_ if x.diferenca > 0 else 0, axis = 1)
-
-df_fumo['media_ (-)'] = df_fumo.apply(lambda x: x.media_ if x.diferenca < 0 else 0, axis = 1)
-
-df_fumo.drop(['media_'], axis = 1, inplace = True)
+t_alpha = t_student.ppf(significancia, graus_de_liberdade)
+# -1.78
 
 
-# T = menor das somas de medias de mesmo sinal
-T = min(df_fumo['media_ (+)'].sum(), df_fumo['media_ (-)'].sum())
-# 22.0
+df_homens = pd.DataFrame(homens)
+df_homens['sexo'] = 'homens'
 
-mu_T = (n * (n + 1)) / 4
-# 315.0
-
-sigma_T = np.sqrt((n * (n + 1) * ((2 * n) + 1)) / 24)
-# 61.053255441458646
+df_mulheres = pd.DataFrame(mulheres)
+df_mulheres['sexo'] = 'mulheres'
 
 
-# Z teste
-Z = (T - mu_T) / sigma_T
-# -4.799088891843698
+df_sexo = df_homens.append(df_mulheres)
+
+df_sexo.reset_index(inplace = True, drop = True)
+
+df_sexo.sort_values(by = 'renda', inplace = True)
+
+df_sexo['ordenacao'] = range(1, len(df_sexo) + 1) # Variável de ordenação
 
 
-# Teste valor crítico z
-if Z >= z_alpha_2 or Z <= -abs(z_alpha_2):
-    print('*' * 50)
-    print('Hipótese H0 rejeitada pelo valor crítico z\nA média de cigarros antes do teste é maior que a média após o teste')
-    print('*' * 50)
-    print('Resultado satisfatório para o novo tratamento')
+df_ordenado = df_sexo[['renda', 'ordenacao']].groupby(['renda']).mean()
+
+df_ordenado.reset_index(inplace = True)
+
+
+df_sexo.drop(['ordenacao'], axis = 1, inplace = True)
+
+df_sexo = df_sexo.merge(df_ordenado, left_on='renda', right_on='renda', how = 'left') # Left join utilizando a renda
+
+
+# R_1 = soma dos postos do grupo n_1
+# R_2 = soma dos postos do grupo n_2
+
+Temp = df_sexo[['sexo', 'ordenacao']].groupby('sexo').sum()
+# homens 61.0
+# mulheres 44.0
+
+R_1 = Temp.loc['homens'][0]
+
+R_2 = Temp.loc['mulheres'][0]
+
+
+u_1 = n_1 * n_2 + ((n_1 * (n_1 + 1)) / (2)) - R_1
+
+
+u_1 = n_1 * n_2 + ((n_1 * (n_1 + 1)) / (2)) - R_1
+# 8.0
+
+u_2 = n_1 * n_2 + ((n_2 * (n_2 + 1)) / (2)) - R_2
+# 40.0
+
+u = min(u_1, u_2)
+
+mu_u = (n_1 * n_2) / 2
+
+sigma_u = np.sqrt(n_1 * n_2 * (n_1 + n_2 + 1) / 12)
+
+Z = (u - mu_u) / sigma_u
+
+Z.round(2)
+# -2.065591117977289
+
+
+# Teste pelo valor crítico t para caso unicaudal inferior
+if Z <= t_alpha:
+    print('Hipótese nula rejeitada pelo teste do valor crítico t\nA média de renda das mulheres é menor que a média de renda dos homens')
 else:
-    print('*' * 50)
-    print('Hipótese H0 aceita pelo valor crítico z\nA média de cigarros antes do teste é menor que a média após o teste')
-    print('*' * 50)
-    print('Resultado insatisfatório para o novo tratamento')
+    print('Hipótese nula aceita pelo teste do valor crítico t\nA média de renda das mulheres é igual a média de renda dos homens')
 
-
-# Teste do valor p
-T, p_valor = wilcoxon(df_fumo['antes'], df_fumo['depois'])
-
-if p_valor <= significancia:
-    print('*' * 50)
-    print('Hipótese H0 rejeitada pelo valor p\nA média de cigarros antes do teste é maior que a média após o teste')
-    print('*' * 50)
-    print('Resultado satisfatório para o novo tratamento')
-else:
-    print('*' * 50)
-    print('Hipótese H0 aceita pelo valor p\nA média de cigarros antes do teste é menor que a média após o teste')
-    print('*' * 50)
-    print('Resultado insatisfatório para o novo tratamento')
+# Hipótese nula rejeitada pelo teste do valor crítico t
+# A média de renda das mulheres é menor que a média de renda dos homens
