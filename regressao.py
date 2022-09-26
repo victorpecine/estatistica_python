e1f3f0cb195b1a1e857f467e4e99c6e128d8341c
Victor Pecine <73555379+victorpecine@users.noreply.github.com>
2022-09-26 18:10:59 -0300

    novo dataframe, estatistica descritiva e analise grafica

--- regressao.py
+++ regressao.py
@@ -1,12 +1,54 @@
+# Fonte: https://www.kaggle.com/dongeorge/beer-consumption-sao-paulo
+
+# Descrição:
+# A cerveja é uma das bebidas mais democráticas e consumidas no mundo. Não sem razão, é perfeito para quase todas as situações, desde o happy hour até grandes festas de casamento.
+
+# Os dados (amostra) foram coletados em São Paulo - Brasil, em uma área universitária, onde existem algumas festas com grupos de alunos de 18 a 28 anos de idade (média).
+
+# Dados:
+# - temp_media - Temperatura Média (°C)
+# - consumo - Consumo de Cerveja (litros)
+
+
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import statsmodels.api as sm
+from scipy.stats import probplot
+import matplotlib.pyplot as plt
 
 
-df_ibge = pd.read_csv('dados/dados.csv') # 76840 linhas, 7 colunas
+df_cervejas = pd.read_csv('dados/dados_projeto.csv', sep=';') # [365 rows x 1 columns]
 
-# Contagem de dados nulos do dataframe
-df_ibge.isnull().sum()
 
-# Ajuste dos nomes das colunas
-df_ibge.columns = df_ibge.columns.str.lower()
+# Estatísticas descritivas
+estat_desc = df_cervejas.describe()
+#                   Y           X
+# count    365.000000  365.000000
+# mean   25401.367123   21.226356
+# std     4399.142703    3.180108
+# min    14343.000000   12.900000
+# 25%    22008.000000   19.020000
+# 50%    24867.000000   21.380000
+# 75%    28631.000000   23.280000
+# max    37937.000000   28.860000
 
-df_ibge.rename(columns={'anos de estudo': 'anos_estudo'}, inplace=True)
+
+# Análise gráfica
+# Boxplot - consumo de litros de cerveja
+ax = sns.boxplot(data=df_cervejas, x='Y', orient='h', width=0.5)
+
+ax.figure.set_size_inches(12, 6)
+
+ax.set_title('Box plot', fontsize=20)
+
+ax.set_xlabel('Consumo de cerveja (litros)', fontsize=16)
+
+# # Temperatura média
+ax = sns.boxplot(data=df_cervejas, x = 'X', orient='h', width=0.5)
+
+ax.figure.set_size_inches(12, 6)
+
+ax.set_title('Box plot', fontsize=20)
+
+ax.set_xlabel('Temperatura média', fontsize=16)
